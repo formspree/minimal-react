2b4882ac33b4c48c0fe51e07c164dbe837d4f6e9
Cole <cole@colevscode.io>
2018-12-28 13:06:36 -0500

    hello world

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Hello extends React.Component {
+  render() {
+      return <h1>Hello world!</h1>;
+  }
+}
+
+function bootstrap() {
+  ReactDOM.render(
+    <Hello />, 
+    document.getElementById("app-container")
+  )
+}
+
+window.addEventListener('load', bootstrap)
